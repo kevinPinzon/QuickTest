aaf639debf172da06464c9b01adea136ba5ee049
kevinPinzon <alxandrp30@gmail.com>
2016-11-05 19:00:01 -0600

    view de resultados, lista

--- imports/ui/resultadosView/resultadosView.js
+++ imports/ui/resultadosView/resultadosView.js
@@ -1,1 +1,12 @@
 import './resultadosView.html';
+import { Calificaciones } from '../../api/calificaciones.js';
+
+Template.resultadosView.onRendered(function(){
+    Meteor.subscribe('calificaciones');
+});
+
+Template.resultadosView.helpers({
+  Resultados: function(){
+    return Calificaciones.find({}).fetch();
+  }
+});
